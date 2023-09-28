1427d4b8ffd2e90e2562304a452df042b0e35b60
Joe Principe <j.principe343@gmail.com>
2023-09-27 22:34:52 -0700

    Started working on board drawing code

--- main.c
+++ main.c
@@ -2,29 +2,41 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int choose_gamemode(int, const char **);
+enum marks {
+    MARK_EMPTY = ' ',
+    MARK_X = 'X',
+    MARK_O = 'O'
+};
+
+enum gamemodes {
+    MODE_LOCAL_CPU = 0,
+    MODE_LOCAL_PVP = 1,
+    MODE_REMOTE_PVP = 2,
+    MODE_QUIT = -1
+};
+
+void init_ncurses(void);
+void init_game(int);
+void place_piece(int, char *, int);
 
 int
 main(void)
 {
     int i;
-    int highlight = 0, choice = 0;
-    int mode_choice = 0;
+    int highlight = 0, opt = 0;
+    int curr_player = 1;
+    char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     bool should_continue = true;
-    WINDOW *mode_win;
-    const char *gamemodes[] = {
+    const char *gamemodes_str[] = {
         "Player vs. CPU",
         "Player vs. Player (Local)",
         "Player vs. Player (Remote)",
         "Quit"
     };
+    WINDOW *mode_win, *board_win;
 
     /* ncurses initialization stuff */
-    initscr();
-    noecho();
-    cbreak();
-    refresh();
-    curs_set(0);
+    init_ncurses();
 
     /* Setup the selection window */
     mode_win = newwin(6, 28, 3, 0);
@@ -36,72 +48,139 @@ main(void)
     mvprintw(1, 0, "Choose a gamemode:");
     refresh();
 
+    /* Choose game mode and start game */
     while (should_continue) {
         for (i = 0; i < 4; i++) {
-            if (i == highlight) { wattron(mode_win, A_REVERSE); }
-            mvwprintw(mode_win, i + 1, 1, "%s", gamemodes[i]);
-            wattroff(mode_win, A_REVERSE);
+            if (i == highlight) { wattron(mode_win, A_STANDOUT); }
+            mvwprintw(mode_win, i + 1, 1, "%s", gamemodes_str[i]);
+            wattroff(mode_win, A_STANDOUT);
         } /* for */
-        choice = wgetch(mode_win);
+        opt = wgetch(mode_win);
 
-        switch (choice) {
+        switch (opt) {
             case KEY_UP:
+            case KEY_LEFT:
+            case 'k':
                 highlight--;
                 if (highlight == -1) { highlight = 3; }
                 break;
             case KEY_DOWN:
+            case KEY_RIGHT:
+            case 'j':
                 highlight++;
                 if (highlight == 4) { highlight = 0; }
                 break;
             /* TODO: Figure out why 10 != KEY_ENTER */
             case 10:
-                mode_choice = choose_gamemode(highlight, gamemodes);
+                init_game(opt);
+                should_continue = false;
+                delwin(mode_win);
+                clear();
                 break;
             default:
                 break;
         } /* switch */
-        should_continue = mode_choice != -1 ? 1 : 0;
     } /* while */
 
-    delwin(mode_win);
+    highlight = 0;
+    board_win = newwin(11, 13, 3, 0);
+    wrefresh(board_win);
+    keypad(board_win, true);
+    curs_set(2);
+
+    /* Replace true with something */
+    while (true) {
+        mvprintw(0, 0, "Player %d's turn:", curr_player);
+        mvprintw(1, 0, "Press ENTER to select a square");
+        refresh();
+
+        mvwprintw(board_win, 0, 0, "     |   |   ");
+        mvwprintw(board_win, 1, 0, "A  %c | %c | %c ", board[0], board[1], board[2]);
+        mvwprintw(board_win, 2, 0, "  ___|___|___");
+        mvwprintw(board_win, 3, 0, "     |   |   ");
+        mvwprintw(board_win, 4, 0, "B  %c | %c | %c ", board[3], board[4], board[5]);
+        mvwprintw(board_win, 5, 0, "  ___|___|___");
+        mvwprintw(board_win, 6, 0, "     |   |   ");
+        mvwprintw(board_win, 7, 0, "C  %c | %c | %c ", board[6], board[7], board[8]);
+        mvwprintw(board_win, 8, 0, "     |   |   ");
+        mvwprintw(board_win, 9, 0, "\n");
+        mvwprintw(board_win, 10, 0, "   1 | 2 | 3 ");
+        wrefresh(board_win);
+
+        for (i = 0; i < 9; i++) {
+            if (i == highlight) { wattron(mode_win, A_REVERSE); }
+            mvwprintw(board_win, 1 + 4 * (i / 3), 3 + 4 * (i % 3), "%c",
+                      board[i]);
+            wattroff(board_win, A_REVERSE);
+        } /* for */
+        opt = wgetch(board_win);
+
+        switch (opt) {
+            case KEY_UP:
+                highlight -= 3;
+                if (highlight < 0) { highlight += 3; }
+                break;
+            case KEY_DOWN:
+                highlight += 3;
+                if (highlight > 8) { highlight -= 3; }
+                break;
+            case KEY_LEFT:
+                highlight--;
+                if (highlight == -1) { highlight = 0; }
+                break;
+            case KEY_RIGHT:
+                highlight++;
+                if (highlight == 9) { highlight = 8; }
+                break;
+            case 10:
+                place_piece(curr_player, board, highlight);
+                break;
+            default:
+                break;
+        } /* switch */
+    } /* while */
+
+    mvprintw(20, 0, "Thanks for playing! Press any key to exit.");
+    refresh();
+    getch();
     endwin();
     return 0;
 }
 
-/* Make this so it chooses the gamemode and whether or not to quit in a logical
- * manner. Rather than returning game mode, it returns whether or not to quit */
-int
-choose_gamemode(int sel, const char **gamemodes)
+void
+init_ncurses(void)
 {
-    int choice;
-    switch (sel) {
-        case 0:
-            /* Start player vs cpu mode */
-            move(9, 0);
-            clrtoeol();
-            printw("You chose %s", gamemodes[sel]);
-            choice = 0;
-            break;
+    initscr();
+    noecho();
+    cbreak();
+    refresh();
+    curs_set(0);
+}
+
+/* This should do some sort of setting up for the game modes */
+void
+init_game(int mode)
+{
+    switch (mode) {
+        case MODE_LOCAL_CPU:
+        case MODE_LOCAL_PVP:
+        case MODE_REMOTE_PVP:
+        case MODE_QUIT:
+            return;
+    } /* switch */
+}
+
+void
+place_piece(int player, char *board, int pos) {
+    switch (player) {
         case 1:
-            /* Start local pvp mode */
-            move(9, 0);
-            clrtoeol();
-            printw("You chose %s", gamemodes[sel]);
-            choice = 1;
+            board[pos] = 'X';
             break;
         case 2:
-            /* Start remote pvp mode */
-            move(9, 0);
-            clrtoeol();
-            printw("You chose %s", gamemodes[sel]);
-            choice = 2;
+            board[pos] = 'O';
             break;
         default:
-            choice = -1;
             break;
     } /* switch */
-    refresh();
-
-    return choice;
 }
 /* EOF */
