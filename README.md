a1ce5f8ad74f6ddf895b98b8a495f2dbede36c43
Joe Principe <j.principe343@gmail.com>
2023-10-01 17:57:25 -0700

    Completed Local PvP Mode

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # multiplayer_tictactoe
 Multiplayer, CLI Tic-Tac-Toe game written in C
 
+# Features
+- [x] Local Player vs. Player Mode
+- [ ] Remote Player vs. Player Mode
+- [ ] Local Player vs. Computer Mode
+
 # Requirements
 * libncurses-dev for ncurses header(s)
-	* Requires `#include <curses.h>` and `-lncurses` during compilation
+	* Requires `#include <ncurses.h>` and `-lncurses` during compilation
--- main.c
+++ main.c
@@ -11,7 +11,9 @@ enum gamemodes {
 
 void init_ncurses(void);
 void init_game(int);
-void place_piece(int, char *, int);
+void print_board(WINDOW *, const char *);
+void place_piece(int *, char *, int, int *);
+int check_for_win(const char *, int);
 
 int
 main(void)
@@ -19,6 +21,8 @@ main(void)
     int i;
     int highlight = 0, opt = 0;
     int curr_player = 1;
+    int turn = 1;
+    int status = -1;
     char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     bool should_continue = true;
     const char *gamemodes_str[] = {
@@ -81,68 +85,79 @@ main(void)
     board_win = newwin(11, 13, 3, 0);
     wrefresh(board_win);
     keypad(board_win, true);
-    curs_set(1);
 
-    /* Replace true with something */
-    while (true) {
-        mvprintw(0, 0, "Player %d's turn:", curr_player);
+    while (status == -1) {
+        mvprintw(0, 0, "Player %d's turn (turn %d):", curr_player, turn);
         mvprintw(1, 0, "Press ENTER to select a square");
         refresh();
 
-        mvwprintw(board_win, 0, 0, "     |   |   ");
-        mvwprintw(board_win, 1, 0, "A  %c | %c | %c ", board[0], board[1], board[2]);
-        mvwprintw(board_win, 2, 0, "  ___|___|___");
-        mvwprintw(board_win, 3, 0, "     |   |   ");
-        mvwprintw(board_win, 4, 0, "B  %c | %c | %c ", board[3], board[4], board[5]);
-        mvwprintw(board_win, 5, 0, "  ___|___|___");
-        mvwprintw(board_win, 6, 0, "     |   |   ");
-        mvwprintw(board_win, 7, 0, "C  %c | %c | %c ", board[6], board[7], board[8]);
-        mvwprintw(board_win, 8, 0, "     |   |   ");
-        mvwprintw(board_win, 9, 0, "\n");
-        mvwprintw(board_win, 10, 0, "   1 | 2 | 3 ");
-        wrefresh(board_win);
+        print_board(board_win, board);
 
         for (i = 0; i < 9; i++) {
-            if (i == highlight) { wattron(mode_win, A_REVERSE); }
+            if (i == highlight) { wattron(board_win, A_STANDOUT); }
             mvwprintw(board_win, 1 + 3 * (i / 3), 3 + 4 * (i % 3), "%c",
                       board[i]);
-            wattroff(board_win, A_REVERSE);
+            wattroff(board_win, A_STANDOUT);
         } /* for */
         opt = wgetch(board_win);
 
         switch (opt) {
             case KEY_UP:
                 highlight -= 3;
-                if (highlight < 0) { highlight += 3; }
+                if (highlight < 0) { highlight += 9; }
                 break;
             case KEY_DOWN:
                 highlight += 3;
-                if (highlight > 8) { highlight -= 3; }
+                if (highlight > 8) { highlight -= 9; }
                 break;
             case KEY_LEFT:
                 highlight--;
-                if (highlight == -1) { highlight = 0; }
+                if (highlight == -1) { highlight = 8; }
                 break;
             case KEY_RIGHT:
                 highlight++;
-                if (highlight == 9) { highlight = 8; }
+                if (highlight == 9) { highlight = 0; }
                 break;
             case 10:
-                place_piece(curr_player, board, highlight);
-                curr_player = curr_player % 2 == 1 ? 2 : 1;
+                place_piece(&curr_player, board, highlight, &turn);
                 break;
             default:
                 break;
         } /* switch */
+
+        /* Check for victory */
+        /* Can only win after the 6th turn, hence the check */
+        if (turn >= 6) { status = check_for_win(board, turn); }
     } /* while */
 
-    mvprintw(20, 0, "Thanks for playing! Press any key to exit.");
+    print_board(board_win, board);
+
+    switch (status) {
+        case 0:
+            mvprintw(15, 0, "The game is a tie!");
+            break;
+        case 1:
+            mvprintw(15, 0, "Player 1 (X) wins!");
+            break;
+        case 2:
+            mvprintw(15, 0, "Player 2 (O) wins");
+            break;
+        default:
+            break;
+    } /* switch */
+
+    mvprintw(16, 0, "Thanks for playing! Press any key to exit.");
     refresh();
     getch();
+
+    delwin(board_win);
+    board_win = NULL;
+
     endwin();
     return 0;
 }
 
+/* Initializes the ncurses stuff */
 void
 init_ncurses(void)
 {
@@ -167,16 +182,87 @@ init_game(int mode)
 }
 
 void
-place_piece(int player, char *board, int pos) {
-    switch (player) {
+print_board(WINDOW *win, const char *board)
+{
+    mvwprintw(win, 0, 0, "     |   |   ");
+    mvwprintw(win, 1, 0, "A  %c | %c | %c ", board[0], board[1], board[2]);
+    mvwprintw(win, 2, 0, "  ___|___|___");
+    mvwprintw(win, 3, 0, "     |   |   ");
+    mvwprintw(win, 4, 0, "B  %c | %c | %c ", board[3], board[4], board[5]);
+    mvwprintw(win, 5, 0, "  ___|___|___");
+    mvwprintw(win, 6, 0, "     |   |   ");
+    mvwprintw(win, 7, 0, "C  %c | %c | %c ", board[6], board[7], board[8]);
+    mvwprintw(win, 8, 0, "     |   |   ");
+    mvwprintw(win, 9, 0, "\n");
+    mvwprintw(win, 10, 0, "   1 | 2 | 3 ");
+    wrefresh(win);
+}
+
+/* Places a piece and increments the turn counter
+ * If the current space is occupied, do nothing */
+void
+place_piece(int *player, char *board, int pos, int *turn)
+{
+    if (board[pos] != ' ') { return; }
+
+    switch (*player) {
         case 1:
             board[pos] = 'X';
+            *player = 2;
             break;
         case 2:
             board[pos] = 'O';
+            *player = 1;
             break;
         default:
             break;
     } /* switch */
+    *turn += 1;
+}
+
+/* Checks the status of the board
+ * Returns -1 if there is no victor yet
+ * Returns  0 if there is a tie (reached turn 10, ie board is full)
+ * Returns  1 if player 1 (X) won
+ * Returns  2 if player 2 (O) won
+ */
+int
+check_for_win(const char *board, int turn)
+{
+    int i;
+
+    /* Check the rows for a victor */
+    for (i = 0; i < 9; i += 3) {
+        if (board[i] == board[i + 1] && board[i + 1] == board[i + 2]) {
+            if (board[i] == 'X') { return 1; }
+            else if (board[i] == 'O') { return 2; }
+        } /* if */
+    } /* for */
+
+    /* Check the columns for a victor */
+    for (i = 0; i < 3; i++) {
+        if (board[i] == board[i + 3] && board[i + 3] == board[i + 6]) {
+            if (board[i] == 'X') { return 1; }
+            else if (board[i] == 'O') { return 2; }
+        } /* if */
+    } /* for */
+
+    /* Checks the backslash diagonal */
+    if (board[0] == board[4] && board[4] == board[8]) {
+        if (board[0] == 'X') { return 1; }
+        else if (board[0] == 'O') { return 2; }
+    } /* if */
+
+    /* Checks the forwardslash diagonal */
+    if (board[6] == board[4] && board[4] == board[2]) {
+        if (board[0] == 'X') { return 1; }
+        else if (board[0] == 'O') { return 2; }
+    } /* if */
+
+    /* Checks if the board is full with no winner */
+    if (turn == 10) { return 0; }
+
+    /* Continues the game */
+    return -1;
 }
 /* EOF */
